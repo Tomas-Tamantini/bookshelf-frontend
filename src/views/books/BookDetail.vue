fed53e2a35ce40af75607ddfa0e240484f38ff34
Tomas Tamantini <tomas.tamantini@petrobras.com.br>
2024-08-20 20:52:56 -0300

    Show book detail

---
+++ src/views/books/BookDetail.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import { getBookById } from '@/services/books'
+import { getAuthorById } from '@/services/authors'
+import { useRoute } from 'vue-router'
+
+const book = ref({
+  id: 0,
+  title: '',
+  year: 0,
+  authorName: ''
+})
+
+const fetchBook = async () => {
+  const { id } = useRoute().params
+  const bookData = await getBookById(Number(id))
+  const author = await getAuthorById(bookData.author_id)
+  book.value = {
+    id: bookData.id,
+    title: bookData.title,
+    year: bookData.year,
+    authorName: author.name
+  }
+}
+
+onMounted(fetchBook)
+</script>
+
+<template>
+  <div>
+    <h1>{{ book.title }}</h1>
+    <p>Author: {{ book.authorName }}</p>
+    <p>Year: {{ book.year }}</p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 2em;
+  margin-bottom: 10px;
+}
+
+p {
+  font-size: 1.2em;
+  line-height: 1.6;
+}
+</style>
